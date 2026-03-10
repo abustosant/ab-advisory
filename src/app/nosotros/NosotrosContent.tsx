d98952acd1a7e7b1da47a6950a922c9dc7437760
abustosant <mail.software@tuta.com>
2026-03-10 11:57:10 -0300

    fix: AnimOnScroll default import en nosotros

--- src/app/nosotros/NosotrosContent.tsx
+++ src/app/nosotros/NosotrosContent.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { AnimOnScroll } from '@/components/AnimOnScroll'
+import AnimOnScroll from '@/components/AnimOnScroll'
 
 const historia = [
   {
